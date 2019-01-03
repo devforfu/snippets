eb6dfe215d7d02562f7f7f9ae4e8fe82fe882910
devforfu <developer.z@outlook.com>
2019-01-03 21:19:11 +0500

    [WIP] Toposort

--- toposort.go
+++ toposort.go
@@ -1,17 +1,55 @@
 // Topological sorting
 package main
 
-func main() {
+import "fmt"
 
+type Graph = map[string][]string
+
+var courses = Graph {
+    "algorithms":            {"data structures"},
+    "calculus":              {"linear algebra"},
+    "compilers":             {"data structures", "formal languages", "computer organization"},
+    "data structures":       {"discrete math"},
+    "databases":             {"data structures"},
+    "discrete math":         {"intro to programming"},
+    "formal languages":      {"discrete math"},
+    "networks":              {"operating systems"},
+    "operating systems":     {"data structures", "computer organization"},
+    "programming languages": {"data structures", "computer organization"},
 }
 
-func TopologicalSort(m map[string][]string) {
-    topoSort(m)
-}
-
-func topoSort(m map[string][]string) []string {
+func (g *Graph) TopologicalSort() []string {
     seen := make(map[string]bool)
     order := make([]string, 0)
+    var visit func (string)
+
+    visit = func (item string) {
+
+    }
+}
+
+func main() {
+    for i, course := range TopologicalSort(courses) {
+        fmt.Printf("%02d: %v\n", i+1, course)
+    }
+}
+
+type Vertex struct {
+    Item string
+    ChildrenCount int
+}
+
+type Vertices = []Vertex
+
+func (vs *Vertices) Len() int { return len(*vs) }
+
+func (vs *Vertices) Less(i, j int) bool { return (*vs)[i].ChildrenCount < (*vs)[j].ChildrenCount }
+
+func (vs *Vertices) Swap(i, j int) { (*vs)[i], (*vs)[j] = (*vs)[j], (*vs)[i] }
+
+func TopologicalSort(m map[string][]string) [] string {
+    seen := make(map[string]bool)
+    order := make([]Vertex, 0)
     var visit func (string)
 
     visit = func (item string) {
@@ -20,11 +58,12 @@ func topoSort(m map[string][]string) []string {
             for _, child := range m[item] {
                 visit(child)
             }
-            order = append(order, item)
+            order = append(order, Vertex{item, len(m[item])})
         }
     }
 
     for key := range m { visit(key) }
+
 
     return order
 }
