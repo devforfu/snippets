ac821b44c59fe0c8248115392573072e180ff56f
devforfu <developer.z@outlook.com>
2019-01-12 21:51:33 +0500

    Struct sort

--- sorting.go
+++ sorting.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "os"
+    "sort"
     "text/tabwriter"
     "time"
 )
@@ -42,6 +43,31 @@ func (x byArtist) Len() int           { return len(x) }
 func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
 func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
 
+type byYear []*Track
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type customSort struct {
+    t []*Track
+    less func(x, y *Track) bool
+}
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
+
 func main() {
-    printTracks(byArtist(tracks))
+    sort.Sort(customSort{tracks, func(x, y *Track) bool {
+        if x.Title != y.Title {
+            return x.Title < y.Title
+        }
+        if x.Year != y.Year {
+            return x.Year < y.Year
+        }
+        if x.Length != y.Length {
+            return x.Length < y.Length
+        }
+        return false
+    }})
+    printTracks(tracks)
 }
