6df5cdc02bc7a3d88c499d6d2cc87bbf2b2d882e
devforfu <developer.z@outlook.com>
2019-01-12 22:31:14 +0500

    More endpoints

--- tabserv.go
+++ tabserv.go
@@ -34,11 +34,31 @@ func createDatabase(tracks []*Track) database {
 }
 
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-   for _, tracks := range db {
-       for _, track := range tracks {
-           fmt.Printf("%v\n", track)
-       }
-   }
+    switch req.URL.Path {
+    case "/list":
+        for _, tracks := range db {
+            for _, track := range tracks {
+                fmt.Printf("%v\n", track)
+            }
+        }
+    case "/find":
+        q := req.URL.Query()
+        title, artist := q.Get("title"), q.Get("artist")
+        if title == "" {
+            fmt.Println("Cannot query songs without title")
+            return
+        }
+        songs, ok := db[title]
+        if !ok {
+            fmt.Printf("Song title is not found: %s", title)
+            return
+        }
+        for _, song := range songs {
+            if artist != "" && song.Artist != artist { continue }
+            fmt.Printf("%v\n", song)
+        }
+    }
+    println()
 }
 
 func main() {
@@ -48,10 +68,10 @@ func main() {
         return d
     }
     tracks := []*Track {
-       {"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
-       {"Go", "Moby", "Moby", 1992, length("3m37s")},
-       {"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
-       {"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+        {"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+        {"Go", "Moby", "Moby", 1992, length("3m37s")},
+        {"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+        {"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
     }
     db := createDatabase(tracks)
     log.Fatal(http.ListenAndServe("localhost:8080", db))
